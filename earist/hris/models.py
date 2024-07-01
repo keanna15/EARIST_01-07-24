f82cf228a11b0b6807b873702e02e0f5fb6324a6
unknown <keannamercado1@gmail.com>
2024-07-01 13:09:39 +0800

    fourth commit

---
+++ earist/hris/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import time
+# Create your models here.
+
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True,unique=True, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(default="Default_pfp.jpg", null=True, blank=True)
+    employee_id = models.CharField(max_length=20,unique=True)
+    surname = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    employment_status = models.CharField(max_length=100, null=True, blank=True)
+    name_ext = models.CharField(max_length=10, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    place_of_birth = models.CharField(max_length=100, blank=True, null=True)
+    sex = models.CharField(max_length=10, blank=True, null=True)
+    civil_status = models.CharField(max_length=20, blank=True, null=True)
+    height_m = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    weight_kg = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    blood_type = models.CharField(max_length=10, blank=True, null=True)
+    gsis_no = models.CharField(max_length=20, blank=True, null=True)
+    pagibig_no = models.CharField(max_length=20, blank=True, null=True)
+    philhealth_no = models.CharField(max_length=20, blank=True, null=True)
+    sss_no = models.CharField(max_length=20, blank=True, null=True)
+    tin_no = models.CharField(max_length=20, blank=True, null=True)
+    agency_em = models.CharField(max_length=100, blank=True, null=True)
+    citizenship = models.CharField(max_length=50, blank=True, null=True)
+    residential_house_no = models.CharField(max_length=10, blank=True, null=True)
+    residential_street = models.CharField(max_length=100, blank=True, null=True)
+    residential_subd = models.CharField(max_length=100, blank=True, null=True)
+    residential_brgy = models.CharField(max_length=100, blank=True, null=True)
+    residential_city = models.CharField(max_length=100, blank=True, null=True)
+    residential_province = models.CharField(max_length=100, blank=True, null=True)
+    residential_zipcode = models.CharField(max_length=10, blank=True, null=True)
+    permanent_house_no = models.CharField(max_length=10, blank=True, null=True)
+    permanent_street = models.CharField(max_length=100, blank=True, null=True)
+    permanent_subd = models.CharField(max_length=100, blank=True, null=True)
+    permanent_brgy = models.CharField(max_length=100, blank=True, null=True)
+    permanent_city = models.CharField(max_length=100, blank=True, null=True)
+    permanent_province = models.CharField(max_length=100, blank=True, null=True)
+    permanent_zipcode = models.CharField(max_length=10, blank=True, null=True)
+    telephone = models.CharField(max_length=20, blank=True, null=True)
+    mobile_number = models.CharField(max_length=20, blank=True, null=True)
+    spouse_surname = models.CharField(max_length=100, blank=True, null=True)
+    spouse_first_name = models.CharField(max_length=100, blank=True, null=True)
+    spouse_middle_name = models.CharField(max_length=100, blank=True, null=True)
+    spouse_name_ext = models.CharField(max_length=10, blank=True, null=True)
+    spouse_occupation = models.CharField(max_length=100, blank=True, null=True)
+    spouse_employer = models.CharField(max_length=100, blank=True, null=True)
+    spouse_business_address = models.CharField(max_length=200, blank=True, null=True)
+    spouse_telephone = models.CharField(max_length=20, blank=True, null=True)
+    elementary_education = models.CharField(max_length=200, blank=True, null=True)
+    secondary_education = models.CharField(max_length=200, blank=True, null=True)
+    father_surname = models.CharField(max_length=100, blank=True, null=True)
+    father_first_name = models.CharField(max_length=100, blank=True, null=True)
+    father_middle_name = models.CharField(max_length=100, blank=True, null=True)
+    father_name_ext = models.CharField(max_length=10, blank=True, null=True)
+    mother_surname = models.CharField(max_length=100, blank=True, null=True)
+    mother_first_name = models.CharField(max_length=100, blank=True, null=True)
+    mother_middle_name = models.CharField(max_length=100, blank=True, null=True)
+    employment_status = models.CharField(max_length=100, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null= True)
+    
+    def __str__(self):
+        return f"{self.employee_id} - {self.first_name} {self.surname}"
+
+
+class OfficialTime(models.Model):
+    employee_id = models.CharField(max_length=20)
+    day = models.CharField(max_length = 10)
+    semester_id = models.CharField(max_length = 10, null= True, blank = True)
+    official_office_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_office_out = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_honorarium_time_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_honorarium_time_out = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_servicecredit_time_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_servicecredit_time_out = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_overtime_time_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    official_overtime_time_out = models.TimeField(default='00:00:00', null=True, blank=True)
+
+
+class AttendanceRecord(models.Model):
+    employee_id = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    time_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    break_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    break_out = models.TimeField(default='00:00:00', null=True, blank=True)
+    time_out = models.TimeField(default='00:00:00', null=True, blank=True)
+    surplusHour_time_in = models.TimeField(default='00:00:00', null=True, blank=True)
+    surplusHour_time_out = models.TimeField(default='00:00:00', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.employee_id} - {self.date}" if self.date else self.employee_id
+
+
+class EditLogs(models.Model):
+    attendance_record = models.ForeignKey(AttendanceRecord, on_delete=models.CASCADE)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    logged_data = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
---
+++ earist/hris/urls.py
@@ -0,0 +1,48 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import *
+
+
+
+urlpatterns = [
+        path('register/', views.registerPage, name='register'),
+        path('login/', views.loginPage, name='login'),
+        path('logout/', views.logoutUser, name='logout'),
+        path("", views.home, name="home"),
+        path('home/', views.user_page, name="user_page"),
+        path('account/', views.accountSettings, name="account"),
+        path('user_account/', views.u_accountSettings, name="u_account"),
+        path('user/', views.userPage, name='user-page'),
+        path('upload/', views.upload_file, name='upload_file'),
+        path('user_upload/', views.u_upload_file, name='u_upload_file'),
+        path('upload/success/', views.upload_success, name='upload_success'),  # Add this line
+        path('search/', views.search_records, name='search_records'),
+        path('user_search/', views.u_search_records, name='u_search_records'),
+        path('dtr/', views.search_attendance, name='search_attendance'),
+        path('user_dtr/', views.u_search_attendance, name='u_search_attendance'),
+        path('search_attendance/', views.search_attendance_record, name='search_attendance_record'),
+        path('edit/<int:record_id>/', views.edit_attendance_record, name='edit_attendance_record'),
+        path('attendance_records/', views.view_attendance_records, name='attendance_records'),
+        path('official_time/', views.official_time_view, name='official_time'),
+        path('edit-logs/', views.edit_logs, name='edit_logs'),
+        path('edit_logs/', views.u_edit_logs, name='u_edit_logs'),
+        path('pds1/', views.pds1, name='pds1'),
+
+]
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  
+
+        # # path("products/", views.products, name='products'),
+        # path("profile/<str:pk_test>/", views.customer, name='customer'),
+        # path('create_order/<str:pk>/', views.createOrder, name='create_order'),
+        # path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+        # path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+        # path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+        # path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+        # path('reset/<uidb64>/<toke>/', auth_views.PasswordResetConfirmView.as_view(), name='passqord_reset_confirm'),
+        # path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+
+
+
